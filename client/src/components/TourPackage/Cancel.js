8859a0033eae23bbea444e40aa037861ad298f62
tharshihan2000@gmail.com <tharshihan_r_e23@engug.ruh.ac.lk>
2025-03-22 14:20:48 +0530

    Added payment success and cancel pages and protected routes

--- client/src/components/TourPackage/Cancel.js
+++ client/src/components/TourPackage/Cancel.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./cancel.css";
 
-function Cancel() {
-  return <div>Cancel</div>;
-}
+const Cancel = () => {
+  const navigate = useNavigate();
 
+  return (
+    <div className="cancel-container">
+      <div className="cancel-box">
+        <div className="cancel-icon">&#10008;</div>
+        <h1>Payment Failed!</h1>
+        <p>
+          Oops! Something went wrong with your transaction. Please try again.
+        </p>
+        <button onClick={() => navigate("/tour")} className="retry-button">
+          Try Again
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Cancel;
